a9fe100a62b7372f4b444abe6d6bc98c63c2accb
Jake Kersten <Kersten.Jacob@gmail.com>
2018-08-11 16:27:14 -0700

    Setting up Planet model and controller. Setting up Shipt model and controller. Setting up initial test planet configuration.

---
+++ LastShot_42/LastShot/Assets/Scripts/Ship/ShipController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    public Vector3 initialThrustDirection;
+    public float initialThrustForce;
+
+    public float rotationSpeed = 1.0f;
+    public ShipModel shipModel;
+
+    private Rigidbody _shipRigidBody;
+
+    public void Start()
+    {
+        _shipRigidBody = shipModel.GetComponent<Rigidbody>();
+
+        if (_shipRigidBody)
+        {
+            _shipRigidBody.AddForce(initialThrustDirection.normalized * initialThrustForce);
+        }
+    }
+
+    public void ApplyGravity(float force, Vector3 position)
+    {
+        if (!_shipRigidBody) return;
+
+
+        Vector3 direction = position - shipModel.transform.position;
+        _shipRigidBody.AddForce(direction.normalized * force);
+    }
+
+}
---
+++ LastShot_42/LastShot/Assets/Scripts/Ship/ShipModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipModel : MonoBehaviour
+{
+    public float forwardThrust = 0.01f;
+    public float mass = 1.0f;
+}
